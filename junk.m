41f466a310804a257858effd88aa7a41e87264fc
Nathan Heavner <nathanheavner@hotmail.com>
2017-02-27 12:02:42 -0700

    debug script in 'junk.m'

--- junk.m
+++ junk.m
@@ -1,9 +1,9 @@
 f = fopen('q0_1core.txt','w');
 
-fprintf(f,'\t no update \t update \n');
+fprintf(f,'\t no update \t update \t nuclear norm\n');
 
 for i=1:length(noupdate_time)
-    fprintf(f,'n = %i:\t %.2e \t %.2e \n',n(i),noupdate_time(i),update_time(i)); 
+    fprintf(f,'n = %i:\t %.2e \t %.2e \t %.2e \n',n(i),noupdate_time(i),update_time(i),nuc_norm_time(i)); 
 end
 
 fclose(f);
