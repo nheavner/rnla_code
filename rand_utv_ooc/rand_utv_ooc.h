0bedc5c9b4553bea4c3a5ab853204d2cfab9caa1
Nathan Heavner <nathan.heavner@colorado.edu>
2017-10-03 09:27:54 -0600

    initialized directory for ooc randqr

--- rand_utv_ooc/rand_utv_ooc.h
+++
@@ -1,7 +0,0 @@
-
-int rand_utv_ooc(
-        int m_A, int n_A, double * buff_A, int ldim_A,
-        int build_u, int m_U, int n_U, double * buff_U, int ldim_U,
-        int build_v, int m_V, int n_V, double * buff_V, int ldim_V,
-        int nb_alg, int pp, int n_iter );
-
